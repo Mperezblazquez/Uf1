419d8ccf0f2387ab65b2c665bfb0f2e33b0e2dad
lolo <lolo@tolonegro>
2014-09-26 16:59:17 +0200

    git inicial

---
+++ src/uf1/M3.java
@@ -0,0 +1,19 @@
+
+package uf1;
+
+
+public class M3 {
+    public static void main(String[] args) {
+        
+        int n1 = 1;
+        int n2 = 1;
+        
+        int resultado;
+        
+        resultado = n1 + n2;
+        
+        System.out.println("sanchez me tiene que dar " + resultado + "€");
+    }
+    
+}
+
